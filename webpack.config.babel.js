9f356a5d9e807e51e0ff7de3c632a7de01e07b53
unprevailing <unprevailing@gmail.com>
2015-08-24 14:08:38 -0600

    added build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,14 @@
+var fs = require('fs');
+var React = require('react');
 var path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var App = require('./app/components/App.jsx');
+var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
@@ -11,21 +18,7 @@ var common = {
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: path.resolve(ROOT_PATH, 'app')
-      }
-    ]
-  },
-  plugins: [
-    new HtmlwebpackPlugin({
-      title: 'Kanban app'
-    })
-  ]
+  }
 };
 
 if(TARGET === 'start' || !TARGET) {
@@ -33,6 +26,10 @@ if(TARGET === 'start' || !TARGET) {
     devtool: 'eval-source-map',
     module: {
       loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css']
+        },
         {
           test: /\.jsx?$/,
           loaders: ['react-hot', 'babel'],
@@ -48,7 +45,75 @@ if(TARGET === 'start' || !TARGET) {
       progress: true
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlwebpackPlugin({
+        title: 'Kanban app'
+      })
     ]
   });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    entry: {
+      app: path.resolve(ROOT_PATH, 'app/main.jsx'),
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: path.resolve(ROOT_PATH, 'build'),
+      filename: 'app.[chunkhash].js'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: path.resolve(ROOT_PATH, 'app')
+        },
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('styles.css'),
+      new Clean(['build']),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        'vendor.[chunkhash].js'
+      ),
+      new webpack.DefinePlugin({
+        'process.env': {
+          // This affects react lib size
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new HtmlwebpackPlugin({
+        title: 'Kanban app',
+        templateContent: renderTemplate(
+          fs.readFileSync(path.join(__dirname, 'templates/index.tpl'), 'utf8'),
+          {
+            app: React.renderToString(<App />)
+          })
+      })
+    ]
+  });
+}
+
+function renderTemplate(template, replacements) {
+  return function() {
+    return template.replace(/%(\w*)%/g, function(match) {
+      var key = match.slice(1, -1);
+
+      return replacements[key] ? replacements[key] : match;
+    });
+  };
 }
