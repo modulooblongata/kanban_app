dde6c1a8b1cbd824bbc994ba528ce13318a089f8
unprevailing <unprevailing@gmail.com>
2015-08-24 12:21:22 -0600

    added dnd

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Editable from './Editable.jsx';
+import Note from './Note.jsx';
+import LaneActions from '../actions/LaneActions';
 
 export default class Notes extends React.Component {
   constructor(props) {
@@ -12,14 +14,14 @@ export default class Notes extends React.Component {
 
     return <ul className='notes'>{notes.map(this.renderNote)}</ul>;
   }
-  renderNote(note) {
+ renderNote(note) {
     return (
-      <li className='note' key={`note${note.id}`}>
+      <Note className='note' onMove={LaneActions.move}
+        data={note} key={`note${note.id}`}>
         <Editable
           value={note.task}
-          onEdit={this.props.onEdit.bind(null, note.id)}
-          onDelete={this.props.onDelete.bind(null, note.id)} />
-      </li>
+          onEdit={this.props.onEdit.bind(null, note.id)} />
+      </Note>
     );
   }
 }
--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import update from 'react/lib/update';
 import NoteStore from './NoteStore';
 
 class LaneStore {
@@ -57,6 +58,8 @@ class LaneStore {
       return;
     }
 
+    this.removeNote(noteId);
+
     const lane = lanes[targetId];
 
     if(lane.notes.indexOf(noteId) === -1) {
@@ -67,6 +70,21 @@ class LaneStore {
     else {
       console.warn('Already attached note to lane', lanes);
     }
+  }
+  removeNote(noteId) {
+    const lanes = this.lanes;
+    const removeLane = lanes.filter((lane) => {
+      return lane.notes.indexOf(noteId) >= 0;
+    })[0];
+
+    if(!removeLane) {
+      return;
+    }
+
+    const removeNoteIndex = removeLane.notes.indexOf(noteId);
+
+    removeLane.notes = removeLane.notes.slice(0, removeNoteIndex).
+      concat(removeLane.notes.slice(removeNoteIndex + 1));
   }
   detachFromLane({laneId, noteId}) {
     const lanes = this.lanes;
@@ -99,6 +117,38 @@ class LaneStore {
     }
 
     return laneIndex;
+  }
+  move({sourceNote, targetNote}) {
+    const lanes = this.lanes;
+    const sourceId = sourceNote.id;
+    const targetId = targetNote.id;
+    const sourceLane = lanes.filter((lane) => {
+      return lane.notes.indexOf(sourceId) >= 0;
+    })[0];
+    const targetLane = lanes.filter((lane) => {
+      return lane.notes.indexOf(targetId) >= 0;
+    })[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+    if(sourceLane === targetLane) {
+      // move at once to avoid complications
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    }
+    else {
+      // get rid of the source
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+
+      // and move it to target
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
+
+    this.setState({lanes});
   }
 }
 
